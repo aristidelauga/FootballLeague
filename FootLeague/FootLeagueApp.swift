11cba65c710a4eccde764f61dfc6182bc593ae03
aristidelauga <aristide.lauga@gmail.com>
2023-06-16 14:17:07 +0200

    first commit

---
+++ FootLeague/FootLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootLeagueApp.swift
+//  FootLeague
+//
+//  Created by Aristide LAUGA on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FootLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
